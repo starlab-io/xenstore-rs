2e2cc97b379bc428747a42059eff6339ef31ed42
Jonathan Creekmore <jonathan.creekmore@starlab.io>
2016-08-19 14:56:12 -0500

    system: re-envision as a system object instead of globals

--- src/system.rs
+++ src/system.rs
@@ -17,113 +17,97 @@
 **/
 
 use std::collections::HashSet;
-use std::sync::Mutex;
 use super::error::Result;
 use super::transaction::*;
 use super::watch::*;
 use super::wire;
 use super::store::*;
 
-use std::ops::{Deref, DerefMut};
-
-lazy_static! {
-    static ref STORE: Mutex<Store> = {
-        Mutex::new(Store::new())
-    };
-
-    static ref WATCHES: Mutex<WatchList> = {
-        Mutex::new(WatchList::new())
-    };
-
-    static ref TXNS: Mutex<TransactionList> = {
-        Mutex::new(TransactionList::new())
-    };
+pub struct System {
+    store: Store,
+    watches: WatchList,
+    txns: TransactionList,
 }
 
-pub fn do_store_mut<F>(dom_id: wire::DomainId,
-                       tx_id: wire::TxId,
-                       thunk: F)
-                       -> Result<HashSet<Watch>>
-    where F: FnOnce(&mut Store, &ChangeSet) -> Result<ChangeSet>
-{
-    // Create unlocked guards for our three "globals"
-    let mut store = STORE.lock().unwrap();
-    let mut watches = WATCHES.lock().unwrap();
-    let mut txns = TXNS.lock().unwrap();
+impl System {
+    pub fn new(store: Store, watches: WatchList, txns: TransactionList) -> System {
+        System {
+            store: store,
+            watches: watches,
+            txns: txns,
+        }
+    }
 
-    let changes = {
-        let root_changeset = ChangeSet::new(store.deref());
+    pub fn do_store_mut<F>(&mut self,
+                           dom_id: wire::DomainId,
+                           tx_id: wire::TxId,
+                           thunk: F)
+                           -> Result<HashSet<Watch>>
+        where F: FnOnce(&mut Store, &ChangeSet) -> Result<ChangeSet>
+    {
+        let changes = {
+            let root_changeset = ChangeSet::new(&self.store);
+            // If the transaction ID is the root transaction
+            let changeset = match tx_id {
+                // return a root changeset
+                ROOT_TRANSACTION => &root_changeset,
+                // otherwise, look up the transaction ID and return that instead
+                _ => try!(self.txns.get(dom_id, tx_id)),
+            };
+
+            // Once we have a changeset, apply the thunk to the data store and
+            // the changeset, returning a new ChangeSet
+            try!(thunk(&mut self.store, changeset))
+        };
+
+        Ok(match tx_id {
+            // If the transaction ID is the root transaction
+            ROOT_TRANSACTION => {
+                // Apply the changes to the data store
+                let applied = self.store.apply(changes);
+                // and fire any watches associated with the changes
+                self.watches.fire(applied)
+            }
+            // otherwise
+            _ => {
+                // just store the changes back with the transaction id
+                try!(self.txns.put(dom_id, tx_id, changes));
+                // and return no watches
+                HashSet::new()
+            }
+        })
+    }
+
+    pub fn do_store<F, R>(&self, dom_id: wire::DomainId, tx_id: wire::TxId, thunk: F) -> Result<R>
+        where F: FnOnce(&Store, &ChangeSet) -> Result<R>
+    {
+        let root_changeset = ChangeSet::new(&self.store);
         // If the transaction ID is the root transaction
         let changeset = match tx_id {
             // return a root changeset
             ROOT_TRANSACTION => &root_changeset,
             // otherwise, look up the transaction ID and return that instead
-            _ => try!(txns.deref().get(dom_id, tx_id)),
+            _ => try!(self.txns.get(dom_id, tx_id)),
         };
 
         // Once we have a changeset, apply the thunk to the data store and
-        // the changeset, returning a new ChangeSet
-        try!(thunk(store.deref_mut(), changeset))
-    };
+        // the changeset, return the result
+        thunk(&self.store, changeset)
+    }
 
-    Ok(match tx_id {
-        // If the transaction ID is the root transaction
-        ROOT_TRANSACTION => {
-            // Apply the changes to the data store
-            let applied = store.deref_mut().apply(changes);
-            // and fire any watches associated with the changes
-            watches.deref_mut().fire(applied)
-        }
-        // otherwise
-        _ => {
-            // just store the changes back with the transaction id
-            try!(txns.deref_mut().put(dom_id, tx_id, changes));
-            // and return no watches
-            HashSet::new()
-        }
-    })
-}
+    pub fn do_watch_mut<F, R>(&mut self, thunk: F) -> R
+        where F: FnOnce(&mut WatchList) -> R
+    {
+        // Do the watch operation
+        thunk(&mut self.watches)
+    }
 
-pub fn do_store<F, R>(dom_id: wire::DomainId, tx_id: wire::TxId, thunk: F) -> Result<R>
-    where F: FnOnce(&Store, &ChangeSet) -> Result<R>
-{
-    // Create unlocked guards for the two "globals" we need
-    let store = STORE.lock().unwrap();
-    let txns = TXNS.lock().unwrap();
-
-    let root_changeset = ChangeSet::new(store.deref());
-    // If the transaction ID is the root transaction
-    let changeset = match tx_id {
-        // return a root changeset
-        ROOT_TRANSACTION => &root_changeset,
-        // otherwise, look up the transaction ID and return that instead
-        _ => try!(txns.deref().get(dom_id, tx_id)),
-    };
-
-    // Once we have a changeset, apply the thunk to the data store and
-    // the changeset, return the result
-    thunk(store.deref(), changeset)
-}
-
-pub fn do_watch_mut<F, R>(thunk: F) -> R
-    where F: FnOnce(&mut WatchList) -> R
-{
-    // Create unlocked guards for the "global" we need
-    let mut watches = WATCHES.lock().unwrap();
-
-    // Do the watch operation
-    thunk(watches.deref_mut())
-}
-
-pub fn do_transaction_mut<F, R>(thunk: F) -> R
-    where F: FnOnce(&mut TransactionList, &mut Store) -> R
-{
-    // Create unlocked guards for the two "globals" we need
-    let mut store = STORE.lock().unwrap();
-    let mut txns = TXNS.lock().unwrap();
-
-    // Do the transaction operation
-    thunk(txns.deref_mut(), store.deref_mut())
+    pub fn do_transaction_mut<F, R>(&mut self, thunk: F) -> R
+        where F: FnOnce(&mut TransactionList, &mut Store) -> R
+    {
+        // Do the transaction operation
+        thunk(&mut self.txns, &mut self.store)
+    }
 }
 
 #[cfg(test)]
@@ -139,24 +123,29 @@ mod test {
         let path = path::Path::try_from(store::DOM0_DOMAIN_ID, "/root/file/path").unwrap();
         let value = store::Value::from("value");
 
+        let mut system = System::new(store::Store::new(),
+                                     watch::WatchList::new(),
+                                     transaction::TransactionList::new());
+
         // set up a watch
-        do_watch_mut(|watch_list| {
+        system.do_watch_mut(|watch_list| {
                 watch_list.watch(store::DOM0_DOMAIN_ID, watch::WPath::Normal(path.clone()))
             })
             .unwrap();
 
         // create a transaction
-        let tx_id = do_transaction_mut(|txlst, store| txlst.start(store::DOM0_DOMAIN_ID, store));
+        let tx_id =
+            system.do_transaction_mut(|txlst, store| txlst.start(store::DOM0_DOMAIN_ID, store));
 
         // add the value in the transaction
-        let fired_watches = do_store_mut(store::DOM0_DOMAIN_ID, tx_id, |store, changes| {
+        let fired_watches = system.do_store_mut(store::DOM0_DOMAIN_ID, tx_id, |store, changes| {
                 store.write(changes, store::DOM0_DOMAIN_ID, path.clone(), value.clone())
             })
             .unwrap();
         assert_eq!(fired_watches.len(), 0);
 
         // end the transaction
-        let changes = do_transaction_mut(|txlst, store| {
+        let changes = system.do_transaction_mut(|txlst, store| {
                 txlst.end(store,
                           store::DOM0_DOMAIN_ID,
                           tx_id,
@@ -165,7 +154,7 @@ mod test {
             .unwrap();
 
         // fire watches
-        let fired_watches = do_watch_mut(|watch_list| watch_list.fire(changes));
+        let fired_watches = system.do_watch_mut(|watch_list| watch_list.fire(changes));
 
         assert_eq!(fired_watches.len(), 1);
     }
