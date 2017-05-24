c93f3885b399ec5df0deb1593d96d3e30a5fffb9
Doug Goldstein <doug@starlab.io>
2017-05-23 22:57:56 -0500

    utils: connect to the XenBus for XenStore

--- rxenstore-utils/src/main.rs
+++ rxenstore-utils/src/main.rs
@@ -1,7 +1,11 @@
 #[macro_use]
 extern crate clap;
+extern crate libxenstore;
+extern crate tokio_core;
 
 use clap::{App, AppSettings, ArgMatches, SubCommand};
+use libxenstore::client::Client;
+use tokio_core::reactor::Core;
 
 
 fn main() {
@@ -15,12 +19,16 @@ fn main() {
         .subcommand(SubCommand::with_name("ls").about("lists available entries"))
         .get_matches();
 
+    let mut core = Core::new().unwrap();
+
+    let cl = Client::connect("/dev/xen/xenbus", &core.handle()).unwrap();
+
     match app_m.subcommand() {
-        ("ls", Some(cmd_m)) => ls_cmd(cmd_m),
+        ("ls", Some(cmd_m)) => ls_cmd(cl, cmd_m),
         _ => unreachable!(),
     }
 }
 
-fn ls_cmd(_: &ArgMatches) {
+fn ls_cmd(_: Client, _: &ArgMatches) {
     println!("ls cmd")
 }
